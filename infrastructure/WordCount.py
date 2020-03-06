f14cf2bb67d4ee51d7e1df93680239bd7eaa141e
NIthin Kumar Guruswamy <42973018+nithinkumar030@users.noreply.github.com>
2020-03-06 14:33:35 -0600

    Create WordCount.py

---
+++ infrastructure/WordCount.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+
+class MRWordCounter(MRJob):
+    def mapper(self, key, line):
+        line = line.strip()
+        words = line.split(' ')
+        for word in words:
+            yield word, 1
+
+    def reducer(self, word, occurences):
+        yield word, sum(occurences)
+
+if __name__ == '__main__':
+    MRWordCounter.run()
